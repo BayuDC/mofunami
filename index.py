dbeff8299411103439b0224918ee556b5dcb92a0
BayuDC <dev.bayudc@gmail.com>
2021-07-24 08:39:32 +0700

    get bot token from env

--- index.py
+++ index.py
@@ -1,17 +1,23 @@
 import os
 
 from discord.ext import commands
-from config import token, prefix
+from config import prefix
 
 bot = commands.Bot(prefix)
+token = os.getenv('BOT_TOKEN')
+
+if not token:
+    print('Bot token not found')
+    exit()
 
 
 @bot.event
 async def on_ready():
     print('Bot is ready')
 
+
 for filename in os.listdir('./cogs'):
-    if(filename.endswith('.py')):
+    if filename.endswith('.py'):
         bot.load_extension(f"cogs.{filename.replace('.py', '')}")
 
 bot.run(token)
