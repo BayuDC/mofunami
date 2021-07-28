7327bd54ebe2364bc4a0918f3f9610dbe20ac314
BayuDC <dev.bayudc@gmail.com>
2021-07-28 11:24:15 +0700

    update command: nekos
    make it better
    add category aliases
    create simple instruction

--- cogs/anime_art.py
+++ cogs/anime_art.py
@@ -1,9 +1,9 @@
-import nekos
-
+from discord import Embed
 from discord.ext import commands
-from discord.ext.commands.errors import CheckFailure
-from data.nekos_life import categories
+from discord.ext.commands.errors import CheckFailure, MissingRequiredArgument
 from utils.special_command import special
+from core.nekos_life import nekos_core
+from config import prefix
 
 
 class AnimeArt(commands.Cog):
@@ -13,17 +13,30 @@ class AnimeArt(commands.Cog):
     @commands.check(special.check)
     @commands.command(aliases=['nekos-life'])
     async def nekos(self, ctx, category):
-        if not category:
-            return await ctx.send('Please enter a specific category')
-        if category not in categories:
-            return await ctx.send('Categories not found!')
+        img_url = nekos_core.get_img(category)
 
-        await ctx.send(nekos.img(category))
+        if img_url is None:
+            return await ctx.send('Category not found!')
+
+        return await ctx.send(img_url)
 
     @nekos.error
     async def nekos_error(self, ctx, error):
         if isinstance(error, CheckFailure):
             return await special.error(ctx)
+        if isinstance(error, MissingRequiredArgument):
+            category = nekos_core.get_category()
+            img_url = nekos_core.get_img('nsfw_avatar')
+
+            embed = Embed(title='Please enter a specific category')
+            embed.set_author(name='Nekos-Life', icon_url=img_url)
+            embed.set_thumbnail(url=img_url)
+
+            embed.add_field(name='Available Categories', value=nekos_core.get_categories(), inline=False)
+            embed.add_field(name='Usage', value=f'`{prefix}{ctx.command.name} <category>`')
+            embed.add_field(name='Example', value=f'`{prefix}{ctx.command.name} {category}`')
+
+            return await ctx.send(embed=embed)
 
         raise error
 
